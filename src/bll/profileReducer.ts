29e4309c0abe3bbfcb67dc9490a5d55cfb4c0c0a
Oleg <shevchenko.oleg.87@gmail.com>
2022-05-29 16:46:09 +0300

    Profile component in progress. Making some fix with validationSchemas.

--- src/bll/profileReducer.ts
+++ src/bll/profileReducer.ts
@@ -1,8 +1,20 @@
+export type UserDataType = {
+    nickname: string | null,
+    email: string | null,
+    avatar: string | undefined,
+}
+type InitStateType = {
+    userData: UserDataType
+}
+const initState = {
+    userData: {
+        nickname: 'orion2004',
+        email: 'orion2004@mail.ru',
+        avatar: "https://png.pngtree.com/png-vector/20190504/ourmid/pngtree-vector-men-avatar-icon-png-image_1020927.jpg",
+    }
+}
 
-
-const initState = {}
-
-export const profileReducer = (state: any = initState, action:any):any => {
+export const profileReducer = (state: InitStateType = initState, action:any):InitStateType => {
     switch (action.type) {
         default: {
             return state
