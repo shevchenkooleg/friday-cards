3a550b3e986c02d1f5924286d9c1bfaaa23ace8e
Oleg <shevchenko.oleg.87@gmail.com>
2022-05-31 00:28:20 +0300

    Make some changes with axios instance for ghPages.

--- src/api/cards-api.ts
+++ src/api/cards-api.ts
@@ -4,8 +4,8 @@ import {UserdataForChangeType} from "../bll/profileReducer";
 
 
 const instance = axios.create({
-    // baseURL:'http://localhost:7542/2.0/',
-    baseURL:'https://github.com/IgnatZakalinsky/cards-nya-back-2-0',
+    baseURL:'http://localhost:7542/2.0/',
+    // baseURL:'https://github.com/IgnatZakalinsky/cards-nya-back-2-0',
     withCredentials: true
 })
 
