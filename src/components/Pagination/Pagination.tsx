0fbb61b1d343e95434670ad5e1be5b3c497e221e
Oleg <shevchenko.oleg.87@gmail.com>
2022-06-07 20:19:23 +0300

    pagination in progress

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Pagination = () => {
+    return (
+        <div>
+
+            <SelectorNumberCards/>
+        </div>
+    );
+};
+
+
+const SelectorNumberCards = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Pagination;
