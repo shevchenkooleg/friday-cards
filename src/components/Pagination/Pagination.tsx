bd052e5b6a0cfab3238d106ee9270add01ac2eb6
Oleg <shevchenko.oleg.87@gmail.com>
2022-06-08 21:36:09 +0300

    Pagination done

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Box from '@mui/material/Box';
+import {FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from '@mui/material';
+import s from './Pagination.module.css'
+import {useAppDispatch, useAppSelector} from "../../bll/store";
+import {
+    getCardsPacksTableTC,
+    SearchSettingsType,
+    setCurrentPageAC, setPageCountAC} from "../../bll/cardPacksReducer";
+import { prepareDataForSearchRequest } from '../../utils/dataPrepare/searchDataPrepare';
 
 const Pagination = () => {
-    return (
-        <div>
 
+
+
+    const dispatch = useAppDispatch()
+    const searchSettings = useAppSelector<SearchSettingsType>((state)=>state.cardPacksReducer.searchSettings)
+    const cardPacksTotalCount = useAppSelector<number | undefined>((state) => state.cardPacksReducer.cardPacksTotalCount)
+    const currentPage = useAppSelector<number | undefined>((state) => state.cardPacksReducer.page)
+    const pageCount = useAppSelector<number | undefined>((state) => state.cardPacksReducer.pageCount)
+
+    useEffect(()=>{
+        dispatch(getCardsPacksTableTC(prepareDataForSearchRequest(searchSettings)))
+    },[searchSettings, pageCount, dispatch])
+
+    useEffect(()=>{
+        dispatch(setCurrentPageAC(1))
+    },[pageCount, dispatch])
+
+    let totalPages: number | undefined
+
+    const getPagesForPagination = () => {
+        totalPages = (cardPacksTotalCount && pageCount && Math.ceil(cardPacksTotalCount / pageCount))
+        let pagesForPagination: number[] = []
+        if (currentPage && currentPage < 6) {
+            for (let i = 1; i <= 11; i++) {
+                pagesForPagination.push(i)
+            }
+        } else if (currentPage && totalPages && (totalPages - currentPage) < 6) {
+
+            for (let i = totalPages - 10; i < totalPages + 1; i++) {
+                pagesForPagination.push(i)
+            }
+        } else if (currentPage) {
+            for (let i = currentPage - 5; i < currentPage + 6; i++) {
+                pagesForPagination.push(i)
+            }
+        }
+        return pagesForPagination
+    }
+
+    const onSpanClickHandler = (page: number) => {
+        dispatch(setCurrentPageAC(page))
+    }
+
+
+    return (
+        <div className={s.content}>
+            <div className={s.pagination}>
+                <span onClick={()=>{dispatch(setCurrentPageAC(1))}}>{'<'}</span>
+                {getPagesForPagination().map((p)=><span onClick={()=>onSpanClickHandler(p)} className={p === currentPage ? s.selected : ''}>{p}</span>)}
+                <span onClick={()=>{cardPacksTotalCount && totalPages && dispatch(setCurrentPageAC(totalPages))}}>{'>'}</span>
+            </div>
             <SelectorNumberCards/>
         </div>
     );
@@ -11,8 +68,37 @@ const Pagination = () => {
 
 
 const SelectorNumberCards = () => {
+
+    const dispatch = useAppDispatch()
+    const [pageAmount, setPageAmount] = React.useState(10);
+
+    useEffect(()=>{
+        dispatch(setPageCountAC(pageAmount))
+    }, [pageAmount, dispatch])
+
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setPageAmount(Number(event.target.value));
+    };
+
+
     return (
-        <div>
+        <div className={s.selector}>
+            <Box sx={{width: 80, margin: '10px 0 10px 0'}} >
+                <FormControl fullWidth>
+                    <InputLabel id="label">Pages</InputLabel>
+                    <Select
+                        value={String(pageAmount)}
+                        onChange={handleChange}
+                        size={"small"}
+                    >
+                        <MenuItem value={5}>5</MenuItem>
+                        <MenuItem value={10}>10</MenuItem>
+                        <MenuItem value={20}>20</MenuItem>
+                        <MenuItem value={50}>50</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
 
         </div>
     );
--- src/utils/dataPrepare/searchDataPrepare.ts
+++ src/utils/dataPrepare/searchDataPrepare.ts
@@ -1,6 +1,6 @@
 import {CardsPacksDataType, SearchSettingsType} from "../../bll/cardPacksReducer";
 
-export const prepareDataForSearchRequest = (searchSettings: SearchSettingsType, searchName: string) => {
+export const prepareDataForSearchRequest = (searchSettings: SearchSettingsType, searchName?: string) => {
     let data = {} as CardsPacksDataType & { minMax?: number | number[] }
     for (let key in searchSettings) {
         if (searchSettings[key as keyof SearchSettingsType]) {
@@ -11,6 +11,6 @@ export const prepareDataForSearchRequest = (searchSettings: SearchSettingsType, 
         data = {...data, min: data.minMax[0], max: data.minMax[1]}
         delete data.minMax
     }
-    if (searchName.trim().length >= 0) data = {...data, packName: searchName} //Добавил для поиска по имени
+    if (searchName && searchName.trim().length >= 0) data = {...data, packName: searchName} //Добавил для поиска по имени
     return data
 }
