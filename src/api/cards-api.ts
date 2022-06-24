d421454d7d7d8f7fd89a6be869453c5861ceaa93
Oleg <shevchenko.oleg.87@gmail.com>
2022-06-24 15:04:05 +0300

    fix bug with smartRandom

--- src/api/cards-api.ts
+++ src/api/cards-api.ts
@@ -18,8 +18,8 @@ import {
 
 
 const instance = axios.create({
-    // baseURL: 'http://localhost:7542/2.0/',
-    baseURL:'https://neko-back.herokuapp.com/2.0',
+    baseURL: 'http://localhost:7542/2.0/',
+    // baseURL:'https://neko-back.herokuapp.com/2.0',
     withCredentials: true
 })
 
